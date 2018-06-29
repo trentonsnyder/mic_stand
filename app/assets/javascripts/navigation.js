bbca6ead600e6dc5a2fb9d540391890d247ebd63
trentonsnyder <trenton.m.snyder@gmail.com>
2018-06-28 20:59:13 -0600

    top nav -> side nav

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,10 +1,20 @@
 App.Nav = {
-  classToggle() {
-    const navs = document.querySelectorAll('.navbar-items')
-    navs.forEach(nav => nav.classList.toggle('navbar-toggle-show'));
+  openMenu() {
+    $('#hamburger').click(function(){
+      $('#navbar').css('display', 'flex');
+    });
+  },
+  closeMenu() {
+    $('#close').click(function(){
+      $('#navbar').css('display', 'none');
+    });
   }
 };
 
-$(document).on("click", "[data-behavior~=toggle-nav]", () => {
-  return App.Nav.classToggle();
+$(document).on("click", "[data-behavior~=open-menu]", () => {
+  return App.Nav.openMenu();
+});
+
+$(document).on("click", "[data-behavior~=close-menu]", () => {
+  return App.Nav.closeMenu();
 });
